6107917bf3c83718ebcfe625eab2225856180a25
unknown <om chavan>
2026-02-27 17:54:24 +0530

    final vite cleanup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // ✅ IMPORTANT
-
+  base: '/',
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  },
   build: {
     outDir: 'dist',
     sourcemap: false
